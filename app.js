37ab248c2c38f6ab96715fec3018e45b02018145
Ulugbek <u.dushanov@gmail.com>
2023-03-03 17:30:27 +0500

    feat: add compute logic

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
-const equalsButtons = document.querySelector('[data-equals]');
-const deleteButtons = document.querySelector('[data-delete]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
@@ -15,11 +16,11 @@ class Calculator {
   clear() {
     this.previousOperand = '';
     this.currentOperand = '';
-    this.operation = undefined;
+    this.operation = null;
   }
 
   delete() {
-
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
   }
 
   appendNumber(number) {
@@ -38,12 +39,61 @@ class Calculator {
   }
 
   compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case '+':
+        computation = prev + current;
+        break;
+      case '−':
+        computation = prev - current;
+        break;
+      case '×':
+        computation = prev * current;
+        break;
+      case '÷':
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
 
+    this.currentOperand = computation;
+    this.previousOperand = '';
+    this.operation = null;
+  }
+
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split('.')[0]);
+    const decimalDigits = stringNumber.split('.')[1];
+    let integerDisplay;
+    
+    if (isNaN(integerDigits)) {
+      integerDisplay = '';
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', {
+        maximumFractionDigits: 0
+      });
+    }
+
+    if (decimalDigits !== undefined) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
   }
 
   updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperand;
-    this.previousOperandTextElement.innerText = this.previousOperand;
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
+
+    if (this.operation !== null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = '';
+    }
   }
 }
 
@@ -63,7 +113,17 @@ operationButtons.forEach(operation => {
   })
 });
 
-equalsButtons.addEventListener('click', () => {
+equalsButton.addEventListener('click', () => {
   calculator.compute();
   calculator.updateDisplay();
-})
+});
+
+allClearButton.addEventListener('click', () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', () => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
